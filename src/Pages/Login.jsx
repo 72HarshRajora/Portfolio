432b00a5d76124e3fee73aa6c49ddf035e23fe07
Harsh Rajora <72harshrajora@gmail.com>
2026-05-26 21:53:16 +0530

    Fix login reload

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -44,6 +44,20 @@ const Login = () => {
         navigate("/admin")
     }
 
+    const verifyToken = async () => {
+        const res = await fetch("https://portfolio-xbxy.onrender.com/admin", {
+            credentials: "include"
+        })
+        if(res.ok){
+            navigate("/admin")
+        }
+    }
+
+    useEffect(() => {
+      verifyToken
+    }, [])
+    
+
     return (
         <div className='login-page'>
             <BgStars />
